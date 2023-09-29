da4e9f3ee05ac9f331593038ae5c91e92daa1931
Koval <kovalevice=artefat@gmail.com>
2023-09-29 14:09:50 +0300

    Delete ununsable fuctions

--- main.py
+++ main.py
@@ -7,70 +7,7 @@ import telebot
 import yt_dlp
 from spotif import music, spotify, search, download, SpotifyOAuth, SpotifyClientCredentials
 
-# Здесь вы можете вызывать импортированные функции
-
-# Здесь вы можете вызывать импортированные функции
-
-# Весь остальной код main.py остается без изменений
-
-# Здесь вы можете вызывать импортированные функции
-
-
 bot=telebot.TeleBot('6673049165:AAEQnWknMKTMQssnrJUwm3yPfGH6pSXtrPc')
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-    btn1 = types.KeyboardButton('Go to the site')
-    btn2 = types.KeyboardButton('Delete photo')
-    btn3 = types.KeyboardButton('Edit text')
-
-    markup.row(btn1)
-    markup.row(btn3, btn2)
-    # file = open('./Haori-Yukata.jpg', 'rb')
-    # bot.send_photo(message.chat.id, file, reply_markup=markup)
-    #bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}', reply_markup=markup)
-    bot.register_next_step_handler(message, on_click)
-
-def on_click(message):
-    if message.text == 'Go to the site':
-        bot.send_message(message.chat.id, 'website is open')
-    elif message.text == 'Delete photo':
-        bot.send_message(message.chat.id, 'Deleted')
-
-@bot.message_handler(commands=['site', 'website'])
-def site(message):
-    webbrowser.open('https://www.youtube.com/watch?v=-l_CYgBj4IE&list=PL0lO_mIqDDFUev1gp9yEwmwcy8SicqKbt&index=3')
-
-
-@bot.message_handler(commands=["start"])
-def main(message):
-    bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}')
-
-@bot.message_handler(commands=["message"])
-def mess(message):
-    bot.send_message(message.chat.id, message)
-
-@bot.message_handler(content_types=['photo', 'audio'])
-def get_photo(message):
-    markup = types.InlineKeyboardMarkup()
-    btn1=types.InlineKeyboardButton('Go to the site', url='google.com')
-    btn2=types.InlineKeyboardButton('Delete photo', callback_data='delete')
-    btn3=types.InlineKeyboardButton('Edit text', callback_data='edit')
-
-
-    markup.row(btn1)
-    markup.row(btn3, btn2)
-    bot.reply_to(message, "фото огонь", reply_markup= markup)
-
-
-@bot.callback_query_handler(func= lambda callback: True)
-def callback_message(callback):
-    if callback.data =='delete':
-        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
-    elif callback.data == 'edit':
-        bot.edit_message_text("Edit text", callback.message.chat.id, callback.message.message_id)
-
 
 import yt_dlp
 
